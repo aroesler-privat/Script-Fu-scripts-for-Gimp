69e7bea3ce51833c84acb043e2e20d8de19401a7
aroesler-privat <aroesler.privat@gmail.com>
2022-04-19 16:48:14 +0200

    changed menu-entry name again

--- gestreift-script-fu-high-pass-sharpen.scm
+++ gestreift-script-fu-high-pass-sharpen.scm
@@ -52,7 +52,7 @@
 ) ;end define
 
 (script-fu-register "gestreift-script-fu-high-pass-sharpen"
-	_"Sharpen (and denoise) ..."
+	_"Sharpen (denoise) ..."
 	"This script ..."
 	"Andreas"
 	"andreas@gestreift.net"
